ec8eedbe4a722656fdeb2de7285bb183277a47cd
AbdelrahmanOsama007 <143852321+AbdelrahmanOsama007@users.noreply.github.com>
2024-09-19 18:57:29 +0300

    final

---
+++ LocalBrand/MyAppContext.sql
@@ -0,0 +1,329 @@
+﻿CREATE TABLE [AspNetRoles] (
+    [Id] nvarchar(450) NOT NULL,
+    [Name] nvarchar(256) NULL,
+    [NormalizedName] nvarchar(256) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [AspNetUsers] (
+    [Id] nvarchar(450) NOT NULL,
+    [FirstName] nvarchar(max) NOT NULL,
+    [DateOfBirth] datetime2 NOT NULL,
+    [UserName] nvarchar(256) NULL,
+    [NormalizedUserName] nvarchar(256) NULL,
+    [Email] nvarchar(256) NULL,
+    [NormalizedEmail] nvarchar(256) NULL,
+    [EmailConfirmed] bit NOT NULL,
+    [PasswordHash] nvarchar(max) NULL,
+    [SecurityStamp] nvarchar(max) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [PhoneNumberConfirmed] bit NOT NULL,
+    [TwoFactorEnabled] bit NOT NULL,
+    [LockoutEnd] datetimeoffset NULL,
+    [LockoutEnabled] bit NOT NULL,
+    [AccessFailedCount] int NOT NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Categories] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Colors] (
+    [Id] int NOT NULL IDENTITY,
+    [ColorName] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Colors] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Orders] (
+    [Id] int NOT NULL IDENTITY,
+    [OrderNumber] nvarchar(450) NOT NULL,
+    [OrderDate] datetime2 NOT NULL,
+    [UserName] nvarchar(max) NOT NULL,
+    [UserEmail] nvarchar(max) NOT NULL,
+    [UserPhone] nvarchar(max) NOT NULL,
+    [OrderStatus] int NOT NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Orders] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Sizes] (
+    [Id] int NOT NULL IDENTITY,
+    [SizeName] nvarchar(max) NOT NULL,
+    [Indicator] int NOT NULL,
+    CONSTRAINT [PK_Sizes] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [AspNetRoleClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [RoleId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserLogins] (
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [ProviderKey] nvarchar(450) NOT NULL,
+    [ProviderDisplayName] nvarchar(max) NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserRoles] (
+    [UserId] nvarchar(450) NOT NULL,
+    [RoleId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserTokens] (
+    [UserId] nvarchar(450) NOT NULL,
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [Name] nvarchar(450) NOT NULL,
+    [Value] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [SubCategories] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [CategoryId] int NOT NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_SubCategories] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_SubCategories_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [Products] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    [Price] decimal(18,2) NOT NULL,
+    [Discount] int NOT NULL,
+    [SubCategoryId] int NOT NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Products_SubCategories_SubCategoryId] FOREIGN KEY ([SubCategoryId]) REFERENCES [SubCategories] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [OrderDetails] (
+    [Id] int NOT NULL IDENTITY,
+    [Price] decimal(18,2) NOT NULL,
+    [SizeId] int NOT NULL,
+    [ColorId] int NOT NULL,
+    [Quantity] int NOT NULL,
+    [ProductId] int NOT NULL,
+    [OrderId] int NOT NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_OrderDetails] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_OrderDetails_Orders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [Orders] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_OrderDetails_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [ProductImages] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [ProductId] int NOT NULL,
+    CONSTRAINT [PK_ProductImages] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ProductImages_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [Stocks] (
+    [Id] int NOT NULL IDENTITY,
+    [Quantity] int NOT NULL,
+    [ProductId] int NOT NULL,
+    [SizeId] int NOT NULL,
+    [ColorId] int NOT NULL,
+    CONSTRAINT [PK_Stocks] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Stocks_Colors_ColorId] FOREIGN KEY ([ColorId]) REFERENCES [Colors] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Stocks_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Stocks_Sizes_SizeId] FOREIGN KEY ([SizeId]) REFERENCES [Sizes] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [ProductColorImages] (
+    [Id] int NOT NULL IDENTITY,
+    [ProductId] int NOT NULL,
+    [ColorId] int NOT NULL,
+    [ImageId] int NOT NULL,
+    CONSTRAINT [PK_ProductColorImages] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ProductColorImages_Colors_ColorId] FOREIGN KEY ([ColorId]) REFERENCES [Colors] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_ProductColorImages_ProductImages_ImageId] FOREIGN KEY ([ImageId]) REFERENCES [ProductImages] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_ProductColorImages_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE NO ACTION
+);
+GO
+
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'IsDeleted', N'Name') AND [object_id] = OBJECT_ID(N'[Categories]'))
+    SET IDENTITY_INSERT [Categories] ON;
+INSERT INTO [Categories] ([Id], [IsDeleted], [Name])
+VALUES (1, CAST(0 AS bit), N'Men'),
+(2, CAST(0 AS bit), N'Women');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'IsDeleted', N'Name') AND [object_id] = OBJECT_ID(N'[Categories]'))
+    SET IDENTITY_INSERT [Categories] OFF;
+GO
+
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ColorName') AND [object_id] = OBJECT_ID(N'[Colors]'))
+    SET IDENTITY_INSERT [Colors] ON;
+INSERT INTO [Colors] ([Id], [ColorName])
+VALUES (1, N'black'),
+(2, N'white'),
+(3, N'red'),
+(4, N'blue');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ColorName') AND [object_id] = OBJECT_ID(N'[Colors]'))
+    SET IDENTITY_INSERT [Colors] OFF;
+GO
+
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Indicator', N'SizeName') AND [object_id] = OBJECT_ID(N'[Sizes]'))
+    SET IDENTITY_INSERT [Sizes] ON;
+INSERT INTO [Sizes] ([Id], [Indicator], [SizeName])
+VALUES (1, 0, N'Small'),
+(2, 0, N'Medium'),
+(3, 0, N'Large'),
+(4, 0, N'XLarge'),
+(5, 0, N'Size32'),
+(6, 0, N'Size34'),
+(7, 0, N'Size36'),
+(8, 0, N'Size38'),
+(9, 0, N'Size40');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Indicator', N'SizeName') AND [object_id] = OBJECT_ID(N'[Sizes]'))
+    SET IDENTITY_INSERT [Sizes] OFF;
+GO
+
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'CategoryId', N'IsDeleted', N'Name') AND [object_id] = OBJECT_ID(N'[SubCategories]'))
+    SET IDENTITY_INSERT [SubCategories] ON;
+INSERT INTO [SubCategories] ([Id], [CategoryId], [IsDeleted], [Name])
+VALUES (1, 1, CAST(0 AS bit), N'T-Shirt'),
+(2, 1, CAST(0 AS bit), N'Hoddie'),
+(3, 1, CAST(0 AS bit), N'Short');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'CategoryId', N'IsDeleted', N'Name') AND [object_id] = OBJECT_ID(N'[SubCategories]'))
+    SET IDENTITY_INSERT [SubCategories] OFF;
+GO
+
+
+CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+GO
+
+
+CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+GO
+
+
+CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+GO
+
+
+CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+GO
+
+
+CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+GO
+
+
+CREATE INDEX [IX_OrderDetails_OrderId] ON [OrderDetails] ([OrderId]);
+GO
+
+
+CREATE INDEX [IX_OrderDetails_ProductId] ON [OrderDetails] ([ProductId]);
+GO
+
+
+CREATE UNIQUE INDEX [IX_Orders_OrderNumber] ON [Orders] ([OrderNumber]);
+GO
+
+
+CREATE INDEX [IX_ProductColorImages_ColorId] ON [ProductColorImages] ([ColorId]);
+GO
+
+
+CREATE INDEX [IX_ProductColorImages_ImageId] ON [ProductColorImages] ([ImageId]);
+GO
+
+
+CREATE INDEX [IX_ProductColorImages_ProductId] ON [ProductColorImages] ([ProductId]);
+GO
+
+
+CREATE INDEX [IX_ProductImages_ProductId] ON [ProductImages] ([ProductId]);
+GO
+
+
+CREATE INDEX [IX_Products_SubCategoryId] ON [Products] ([SubCategoryId]);
+GO
+
+
+CREATE INDEX [IX_Stocks_ColorId] ON [Stocks] ([ColorId]);
+GO
+
+
+CREATE INDEX [IX_Stocks_ProductId] ON [Stocks] ([ProductId]);
+GO
+
+
+CREATE INDEX [IX_Stocks_SizeId] ON [Stocks] ([SizeId]);
+GO
+
+
+CREATE INDEX [IX_SubCategories_CategoryId] ON [SubCategories] ([CategoryId]);
+GO
