d22a6430c81bdf32184d496d5be7fa661a435eb8
Yasser <yasserhhh161@gmail.com>
2025-01-15 21:17:34 +0200

    gg

--- LocalBrand/Controllers/WebHookController.cs
+++ LocalBrand/Controllers/WebHookController.cs
@@ -6,13 +6,47 @@ using Infrastructure.IGenericRepository;
 using Infrastructure.IRepository;
 using Infrastructure.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Model.Enums;
 using Model.Models;
+using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace LocalBrand.Controllers
 {
+    public class SignatureValidator
+    {
+        public bool ValidateSignature(dynamic requestBody, string receivedSignature, string secretKey)
+        {
+            string path = "";
+            foreach (string key in requestBody.data.signatureKeys)
+            {
+                path += "&" + key + "=" + requestBody.data[key];
+            }
+            string message = path.Length > 0 ? path.Substring(1) : string.Empty;
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = encoding.GetBytes(secretKey);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (HMACSHA256 hmacmd256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashMessage = hmacmd256.ComputeHash(messageBytes);
+                string computedSignature = ByteToString(hashMessage).ToLower();
+                return receivedSignature.Equals(computedSignature);
+            }
+        }
+        private string ByteToString(byte[] buffer)
+        {
+            StringBuilder hex = new StringBuilder(buffer.Length * 2);
+            foreach (byte b in buffer)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+    }
     [Route("api/[controller]")]
     [ApiController]
     public class WebHookController : Controller
@@ -31,55 +65,43 @@ namespace LocalBrand.Controllers
         }
         [HttpPost("CompletePayment")]
         [EnableRateLimiting("CompletePaymentPolicy")]
-        public async Task<IActionResult> CompletePayment([FromForm] string paymentStatus, [FromForm] string merchantOrderId)
+        public async Task<IActionResult> CompletePayment([FromForm] object data)
         {
             try
             {
-                var result = await _orderrepository.GetByIdAsync(int.Parse(merchantOrderId));
-                var orderobject = (Order)result.Data;
-                if (paymentStatus == "SUCCESS")
+                string requestBody;
+                using (var reader = new System.IO.StreamReader(Request.Body))
                 {
-                    if (result.Success)
-                    {
-                        orderobject.IsTransactionSuccess = true;
-                        await _orderrepository.SaveChangesAsync();
-                        var orderdto = new OrderDto() { FirstName = orderobject.UserAddress.FirstName, LastName = orderobject.UserAddress.LastName, Email = orderobject.UserAddress.Email};
-                        _orderService.SendOrderProcessedEmail(orderdto,orderobject.OrderNumber);
-                       return Ok(new OperationResult { Success = true, Data = true, Message = "Ordered Successfully" }) ;
-                    }
-                    else
-                    {
-                        return Ok(result);
-                    }
+                    requestBody = reader.ReadToEnd();
                 }
-                else
-                {
-                    if (result.Success)
-                    {
-                        foreach (var item in orderobject.OrderDetails)
-                        {
-                            var productresult = await _productrepository.GetByIdAsync(item.Product.Id);
-                            Product product;
-                            if (productresult.Success)
-                            {
-                                product = (Product)productresult.Data;
-                                var resulttt = product.Stock.FirstOrDefault(s => s.SizeId == item.SizeId && s.ColorId == item.ColorId);
-                                if (resulttt != null)
-                                {
-                                    resulttt.Quantity = resulttt.Quantity + item.Quantity;
-                                    await _productrepository.SaveChangesAsync();
-                                }
-                            }
+                string receivedSignature = Request.Headers["x-kashier-signature"];
+                string secretKey = "224067ad-549d-41e0-a1b0-093ee0b996a0";
+                dynamic parsedBody = JObject.Parse(requestBody);
 
-                        }
-                        await _orderrepository.DeleteAsync(int.Parse(merchantOrderId));
-                        return Ok(new OperationResult { Success = false, Data = false, Message = "Transaction Failed" });
-                    }
-                    else
+                SignatureValidator validator = new SignatureValidator();
+                bool isSignatureValid = validator.ValidateSignature(parsedBody, receivedSignature, secretKey);
+                if (isSignatureValid)
+                {
+                    var kashirobject = (PaymentDto)data;
+                    var result = await _orderrepository.GetByIdAsync(int.Parse(kashirobject.merchantOrderId));
+                    var orderobject = (Order)result.Data;
+                    if (kashirobject.status == "SUCCESS")
                     {
-                        return Ok(result);
+                        if (result.Success)
+                        {
+                            orderobject.IsTransactionSuccess = true;
+                            await _orderrepository.SaveChangesAsync();
+                            var orderdto = new OrderDto() { FirstName = orderobject.UserAddress.FirstName, LastName = orderobject.UserAddress.LastName, Email = orderobject.UserAddress.Email };
+                            _orderService.SendOrderProcessedEmail(orderdto, orderobject.OrderNumber);
+                            return Ok(new OperationResult { Success = true, Data = true, Message = "Ordered Successfully" });
+                        }
+                        else
+                        {
+                            return Ok(result);
+                        }
                     }
                 }
+                return Ok(new OperationResult { Success = false, Data = false, Message = "Transaction Failed" });
             }
             catch (Exception ex)
             {
